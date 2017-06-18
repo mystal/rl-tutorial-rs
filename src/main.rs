201ea6c690f063c4ae5591d95e6ac3e7d07764a1
Gabriel Martinez <reitaka@gmail.com>
2017-06-18 13:45:46 -0700

    Base window with '@' drawn to the screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate tcod;
+
+use tcod::{colors, BackgroundFlag, Console, RootConsole};
+use tcod::input::KeyCode;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const LIMIT_FPS: u32 = 20;
+
 fn main() {
-    println!("Hello, world!");
+    let mut root = RootConsole::initializer()
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust libtcod tutorial")
+        .font("assets/arial10x10.png", tcod::FontLayout::Tcod)
+        .font_type(tcod::FontType::Greyscale)
+        .init();
+
+    let mut player_x = 1;
+    let mut player_y = 1;
+
+    while !root.window_closed() {
+        root.set_default_foreground(colors::WHITE);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        root.flush();
+
+        match root.wait_for_keypress(true).code {
+            KeyCode::Escape => break,
+            _ => {},
+        }
+    }
 }
